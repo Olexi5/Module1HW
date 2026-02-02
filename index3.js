2720d5499a0a05cd6fe6128ab18174dff26f3cfe
Olexi5 <sashkov2012@gmail.com>
2026-02-02 20:54:07 +0200

    did HW4

--- index3.js
+++ index3.js
@@ -1,18 +1,18 @@
-"use strict";
-const result = 5 + 5 + '5'
-console.log(result)
-console.log(typeof(result))
+// "use strict";
+// const result = 5 + 5 + '5'
+// console.log(result)
+// console.log(typeof(result))
 
-const email = "sashkov2012@gmail.com";
-console.log(email.includes("@"))
-console.log(email.length)
+// const email = "sashkov2012@gmail.com";
+// console.log(email.includes("@"))
+// console.log(email.length)
 
-const my = "My"
-const name = "name";
-const is = "is";
-const fullName = my + name + is + 'Viktor'
-console.log(fullName)
+// const my = "My"
+// const name = "name";
+// const is = "is";
+// const fullName = my + name + is + 'Viktor'
+// console.log(fullName)
 
-const userName = "Олександра"
-const payment = 300
-alert(`Дякуємо, ${userName}! До сплати ${payment} гривень.`)
+// const userName = "Олександра"
+// const payment = 300
+// alert(`Дякуємо, ${userName}! До сплати ${payment} гривень.`)
