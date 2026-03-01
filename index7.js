361a3d4b1befa2fd96c1321f78c953220d111d14
Olexi5 <sashkov2012@gmail.com>
2026-03-01 11:32:20 +0200

    hw-8 done

--- index7.js
+++ index7.js
@@ -1,45 +1,45 @@
 "use strict";
 
-//1
-const arr1 = [1, 2, 3];
-arr1[1] = 10;
-console.log(arr1);
-//2
-const arr2 = ["hi", "bro", "hello"];
-arr2.push("friends");
-console.log(arr2);
-//3
-const arr3 = [6, 10, 49, 78];
-let sum = 0;
-for (let item of arr3) {
-  sum += item;
-}
-console.log(sum);
-//4
-const arr4 = [1,2,3,4,5]
-for(let i = 0; i<arr4.length; i++){
-    console.log(arr4[i])
-}
-//5
-const arr5 = ["hello", "hi", "rock", "friends", "interesting"]
-for (let string of arr5){
-    if (string.length>5){
-        console.log(string)
-    }
-}
-//6
-const nums = [43,3,56,23,8,87,45,9,93,85]
-let max = nums[0]
-for (let num of nums){
-    if(num>=max){
-        max=num
-    }
-}
-console.log("max = ",max)
-//7
-const nums2 = [43,93,86,74,6,7,83,6,94,85]
-for (let num2 of nums2){
-    if (num2%2 ===0){
-        console.log(num2)
-    }
-}
+// //1
+// const arr1 = [1, 2, 3];
+// arr1[1] = 10;
+// console.log(arr1);
+// //2
+// const arr2 = ["hi", "bro", "hello"];
+// arr2.push("friends");
+// console.log(arr2);
+// //3
+// const arr3 = [6, 10, 49, 78];
+// let sum = 0;
+// for (let item of arr3) {
+//   sum += item;
+// }
+// console.log(sum);
+// //4
+// const arr4 = [1,2,3,4,5]
+// for(let i = 0; i<arr4.length; i++){
+//     console.log(arr4[i])
+// }
+// //5
+// const arr5 = ["hello", "hi", "rock", "friends", "interesting"]
+// for (let string of arr5){
+//     if (string.length>5){
+//         console.log(string)
+//     }
+// }
+// //6
+// const nums = [43,3,56,23,8,87,45,9,93,85]
+// let max = nums[0]
+// for (let num of nums){
+//     if(num>=max){
+//         max=num
+//     }
+// }
+// console.log("max = ",max)
+// //7
+// const nums2 = [43,93,86,74,6,7,83,6,94,85]
+// for (let num2 of nums2){
+//     if (num2%2 ===0){
+//         console.log(num2)
+//     }
+// }
---
+++ index8.js
@@ -0,0 +1,26 @@
+"use strict"
+//1
+const friends = ['Margo', 'Poly', 'Kiwi', 'Ajax']
+let str1 = ''
+let str2 = ''
+for(let i = 0; i<friends.length; i++){
+    str1+=friends[i]
+    str1+=","
+}
+str2 = friends.join(",")
+console.log("with for - ", str1)
+console.log("with join - ", str2);
+//2
+const cards = ["Card-1", "Card-2", "Card-3", "Card-4", "Card-5"];
+//3
+const cardToRemove = "Card-3"
+cards.splice(cards.indexOf(cardToRemove), 1)
+console.log(cards)
+//4
+const cardToInsert = "Card-6"
+cards.splice(cards.length, 0, cardToInsert)
+console.log(cards)
+//5
+const cardToUpdate = "Card-4"
+cards.splice(cards.indexOf(cardToUpdate), 1, cardToUpdate+"👀")
+console.log(cards)
